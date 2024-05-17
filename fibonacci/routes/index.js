fafbda46a7dc25622a9da41ebf1a16364c5473b4
BitHero <nuhumusah874@gmail.com>
2024-05-17 16:37:57 +0000

    Fibonacci router

--- fibonacci/routes/index.js
+++ fibonacci/routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Welcome to the Fibonacci Calculator' });
 });
+
+router.get('/error', function(req, res, next) {
+  next({
+    status: 404,
+    message: "Fake error"
+  })
+})
 
 module.exports = router;
